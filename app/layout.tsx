f18dd1f45350617d65bb490e9cceebe649d8038b
Fergyuson <magicklessons@gmail.com>
2026-01-16 22:28:30 +0300

    feat: add Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { ReactNode } from 'react';
 import { StoreProvider } from '@/app/providers/StoreProvider/ui/StoreProvider';
 import '@/app/styles/index.scss';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	icons: {
+		icon: [{ url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' }]
+	}
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
